c1ea7cb4639943c8ec087d3ceaba6d64601395b4
ady suryadi <ady.limmo@gmail.com>
2024-07-17 19:46:01 +0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+    description = "Masukan access key pada file terraform.tfvars"
+    type = string
+}
+variable "secret_key" {
+    description = "Masukan secret key pada file terraform.tfvars"
+    type = string
+}
+variable "region" {
+    description = "Masukan region pada file terraform.tfvars"
+    type = string
+}
